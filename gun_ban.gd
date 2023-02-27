6c34843b42d9db702dfab106a8250038bfd70556
solitaryflyingbird <fkdlcb777@gmail.com>
2023-02-27 18:31:55 +0900

    흰건반 모듈화

--- gun_ban.gd
+++ gun_ban.gd
@@ -4,16 +4,32 @@ extends Area2D
 var in_mouse = 0
 
 # Called when the node enters the scene tree for the first time.
+var player
+var timer
 func _ready():
-	pass # Replace with function body.
+	player = $player
+	timer = $Timer
+func play_node():
+	player.play()
+	timer.start()
+func stop_node():
+	print(timer.time_left)
+	player.stop()
+	timer.stop()
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
 		if in_mouse:
+			play_node()
 			print("click")
+	elif player.playing:
+		stop_node()
 
 func _on_gun_ban_mouse_entered():
 	in_mouse = 1
 
 func _on_gun_ban_mouse_exited():
 	in_mouse = 0
+
+func _on_Timer_timeout():
+	stop_node()
