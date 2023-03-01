ba6a584fb9419e6b79d3ae669d57e96b598ca9dc
solitaryflyingbird <fkdlcb777@gmail.com>
2023-03-01 21:42:56 +0900

    음 안겹치게

---
+++ 검은건반/blac_gun_ban.gd
@@ -0,0 +1,36 @@
+extends Area2D
+
+
+export var in_mouse = 0
+export(Resource) var note_sound
+# Called when the node enters the scene tree for the first time.
+var player
+var timer
+func _ready():
+	player = $player
+	player.stream = note_sound
+	timer = $Timer
+func play_node():
+	player.play()
+	timer.start()
+func stop_node():
+	var length
+	length = 2 - timer.time_left
+	player.stop()
+	timer.stop()
+
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
+		if in_mouse:
+			play_node()
+	elif player.playing:
+		stop_node()
+
+func _on_gun_ban_mouse_entered():
+	in_mouse = 1
+
+func _on_gun_ban_mouse_exited():
+	in_mouse = 0
+
+func _on_Timer_timeout():
+	stop_node()
--- 흰건반/gun_ban.gd
+++ 흰건반/gun_ban.gd
@@ -2,26 +2,48 @@ extends Area2D
 
 
 var in_mouse = 0
+var black_crash = 0
 export(Resource) var note_sound
+export(NodePath) var black_gun
+export(NodePath) var black_gun2
 # Called when the node enters the scene tree for the first time.
 var player
 var timer
+var black_in_mouse
 func _ready():
 	player = $player
 	player.stream = note_sound
 	timer = $Timer
+	
+	if black_gun != null:
+		black_gun = get_node(black_gun)
+	if black_gun2 != null:
+		black_gun2 = get_node(black_gun2)
+
 func play_node():
 	player.play()
 	timer.start()
+
+
 func stop_node():
 	var length
 	length = 2 - timer.time_left
-	print(length)
+
 	player.stop()
 	timer.stop()
 
+func _gun_check():
+	if black_gun != null or black_gun2 != null:
+		if black_gun != null and black_gun.in_mouse == 1:
+			return 1
+		if black_gun2 != null and black_gun2.in_mouse ==1:
+			return 1
+	return 0 
+
 func _input(event):
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
+	black_in_mouse = _gun_check()
+
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed and black_in_mouse==0:
 		if in_mouse:
 			play_node()
 	elif player.playing:
