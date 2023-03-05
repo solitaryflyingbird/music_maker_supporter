56f592725c374c7681fcd01716cab7fa2c488569
solitaryflyingbird <fkdlcb777@gmail.com>
2023-03-05 19:29:12 +0900

    지우기

--- 건반판/note_array.gd
+++ 건반판/note_array.gd
@@ -7,14 +7,20 @@ onready var note_label = get_node("note_label")
 func _array_append(n):
 	note_array.append(n)
 	note_label.text_append(str(n))
-
-
+func array_remove():
+	var array_length = note_array.size()
+	print(array_length)
+	if array_length > 0:
+		note_array.remove(array_length - 1)
+		var new_text = ""
+		for i in range(note_array.size()):
+			new_text += str(note_array[i]) + " "
+		note_label.text = new_text
+		note_label.text_remove()
 
 func _ready():
 	print(note_array)
-	pass # Replace with function body.
+	pass 
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
 #	pass
--- 건반판/note_label.gd
+++ 건반판/note_label.gd
@@ -1,19 +1,16 @@
 extends Label
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 func text_append(new_text: String):
 	text += new_text
 func _text_change(n):
 	text = n
-# Called when the node enters the scene tree for the first time.
+	
+func text_remove():
+	if text != "":
+		text = text.substr(0, text.length() - 1)
+
 func _ready():
+	text = ""
 
-	text = "Hello, world!"
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
