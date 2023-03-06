bc54f1cc35e87db02581a9f5a1f5832887c4e3a9
solitaryflyingbird <fkdlcb777@gmail.com>
2023-03-06 20:33:05 +0900

    검은건반도 지워지게

--- 건반판/note_array.gd
+++ 건반판/note_array.gd
@@ -6,17 +6,13 @@ onready var note_label = get_node("note_label")
 # Called when the node enters the scene tree for the first time.
 func _array_append(n):
 	note_array.append(n)
-	note_label.text_append(str(n))
+	note_label.array_to_text(note_array)
 func array_remove():
 	var array_length = note_array.size()
 	print(array_length)
 	if array_length > 0:
 		note_array.remove(array_length - 1)
-		var new_text = ""
-		for i in range(note_array.size()):
-			new_text += str(note_array[i]) + " "
-		note_label.text = new_text
-		note_label.text_remove()
+		note_label.array_to_text(note_array)
 
 func _ready():
 	print(note_array)
--- 건반판/note_label.gd
+++ 건반판/note_label.gd
@@ -5,12 +5,18 @@ func text_append(new_text: String):
 	text += new_text
 func _text_change(n):
 	text = n
+func array_to_text(array):
+	var my_string = ""
+	for i in array:
+		my_string = my_string + str(i)
+	text = my_string
 	
 func text_remove():
 	if text != "":
 		text = text.substr(0, text.length() - 1)
+	print(text)
 
 func _ready():
 	text = ""
-
+	text_remove()
 
