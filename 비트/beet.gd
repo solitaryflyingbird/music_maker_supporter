525b493beb59c05224e074c1bc435869563bf4f3
solitaryflyingbird <fkdlcb777@gmail.com>
2023-03-10 21:31:17 +0900

    쉼표까지:

--- 비트/beet.gd
+++ 비트/beet.gd
@@ -2,10 +2,11 @@ extends Node2D
 
 var player
 var play_timer
+var rest_timer
 var note_player 
 var is_space_pressed = false
 var is_playing = false
-
+var note_length_arr = []
 
 
 export(NodePath) var note_arr
@@ -17,8 +18,10 @@ func _ready():
 	player = $player
 	play_timer = $play_timer
 	note_player = $note_player
+	rest_timer = $rest_timer
 	
 	######
+
 	note_arr = get_node(note_arr)
 	note_arr = note_arr.note_arr
 	now_note = note_player.note_dict[note_arr[0]]
@@ -55,6 +58,11 @@ func play_note():
 		player.play()
 		is_playing = true
 		is_space_pressed = true
+		if note_bar == 0:
+			return;
+		var rest_length = (2 - rest_timer.time_left)*8
+		rest_length = (round_int(rest_length))
+		note_length_arr.append(rest_length)
 		
 func round_int(number):
 	var remainder = int(number*10) % 10
@@ -67,7 +75,10 @@ func stop_note():
 	player.stop()
 	var note_length = (2 - play_timer.time_left)*8
 	note_length = (round_int(note_length))	
+	note_length_arr.append([note_arr[note_bar], note_length])
+	print(note_length_arr)
 	play_timer.stop()
+	rest_timer.start()
 	is_playing = false
 	next_note()
 
