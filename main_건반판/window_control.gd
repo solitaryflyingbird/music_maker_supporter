70613538549c042bd672832e3da911927c6ff7a0
solitaryflyingbird <fkdlcb777@gmail.com>
2023-03-22 21:42:10 +0900

    텍스트 정보 입력까지

--- main_건반판/window_control.gd
+++ main_건반판/window_control.gd
@@ -1,10 +1,17 @@
 extends Control
 
 onready var my_window = $MyWindow
-
+onready var file_name = $MyWindow/file_name
 func _ready():
 	pass
 func open_window():
 	my_window.popup_centered()
 func close_window():
 	my_window.hide()
+func print_text():
+	var x = file_name.text
+	print(x)
+
+
+func _on_save_button_pressed():
+	print_text()
