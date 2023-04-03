112e788a3447db4ec4a695cba9a77a569aa1370c
solitaryflyingbird <fkdlcb777@gmail.com>
2023-04-03 22:32:19 +0900

    불러오기 자잘오류수정

--- 불러오기/board_control.gd
+++ 불러오기/board_control.gd
@@ -1,15 +1,29 @@
 extends Control
 
+
 # Your list of items
-var items = ["AAAA", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var items = ["save file is not exist"]
 
 # Reference to the GridContainer
 onready var grid_container = $ScrollContainer/GridContainer
 
 func _ready():
 	create_board()
+func remove_all_grids():
+	for child in grid_container.get_children():
+		grid_container.remove_child(child)
+		child.queue_free()
 
 func create_board():
+	remove_all_grids()
+	var parent_node = get_parent()
+	if parent_node.file_names_and_contents_dic:
+		items = []
+		for key in parent_node.file_names_and_contents_dic:
+			print(key)
+			items.append(key)
+	else:
+		items = ["save file is not exist"]
 	# Set the minimum button size
 	var button_min_width = 380
 	var button_min_height = 50
@@ -20,7 +34,8 @@ func create_board():
 		button.rect_min_size = Vector2(button_min_width, button_min_height) # Set the minimum size
 		button.connect("pressed", self, "_on_button_pressed", [item])
 		grid_container.add_child(button)
-
-
+	
+	
 func _on_button_pressed(item):
+	create_board()
 	print("Button for item %s was pressed." % item)
--- 불러오기/lord_text.gd
+++ 불러오기/lord_text.gd
@@ -1,11 +1,12 @@
 extends Node
 var load_data_index = []
+var file_names_and_contents
+var file_names_and_contents_dic
 func _ready():
 	process_folder()
-	print(load_data_index)
 
 func process_folder():
-	# Replace this with the path to the folder you want to access
+	file_names_and_contents_dic = {}
 	var folder_path := "res://save/"
 	
 	# Create a Directory instance and open the folder
@@ -15,17 +16,16 @@ func process_folder():
 	# Check if the folder was opened successfully
 	if err == OK:
 		# Get the list of file names and contents
-		var file_names_and_contents := get_text_files_contents(dir, folder_path)
-		
-		print(file_names_and_contents)
+		file_names_and_contents = get_text_files_contents(dir, folder_path)
 		for file_data in file_names_and_contents:
-			print("File Name: ", file_data.name, " Content: ", file_data.content)
+			file_names_and_contents_dic[file_data.name] = file_data.content
 			load_data_index.append([file_data.name, file_data.content])
 	else:
 		print("Error opening folder:", folder_path)
 
 func get_text_files_contents(dir: Directory, folder_path: String) -> Array:
 	var file_data_array := []
+	
 	
 	# List all the files in the folder
 	dir.list_dir_begin()
@@ -34,20 +34,16 @@ func get_text_files_contents(dir: Directory, folder_path: String) -> Array:
 	while file_name != "":
 		if !dir.current_is_dir():
 			# Check if the file has a '.txt' extension
-
 			# Read the file's content
 			var file_path := folder_path.plus_file(file_name)
 			var file := File.new()
 			file.open(file_path, File.READ)
 			var file_content := file.get_as_text()
 			file.close()
-			
-			# Store the file's name and content in a Dictionary and add it to the array
-			file_data_array.append({"name": file_name, "content": file_content})
-		
+			if len(file_content) != 0:
+				file_data_array.append({"name": file_name, "content": file_content})
 		file_name = dir.get_next()
 	
 	# Cleanup
 	dir.list_dir_end()
-	
 	return file_data_array
